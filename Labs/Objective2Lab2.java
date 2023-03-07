b4103b5bb7fc7542c848e75341a0ec2ec5a67ac5
Spencer Williams <srwilliams349@gmail.com>
2023-03-07 18:02:03 -0500

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] agrs) {
+		String name = "Spencer Williams";
+		int age = 32;
+		String state = "Delaware";
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}
