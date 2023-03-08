4f96afcea757a8fd0b9241bfba5928c43a0c6d0a
Spencer Williams <srwilliams349@gmail.com>
2023-03-07 21:27:07 -0500

    finish Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1990;
+		String month = "November";
+		boolean weekend = true;
+		boolean week = false;
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(week);
+		//Declare a variable to store whether today is a weekend or not
+	}
+}
