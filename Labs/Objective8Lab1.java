c3ac6e4bb5427cb8782931000b6631c9f05130d3
Spencer Williams <srwilliams349@gmail.com>
2023-03-09 12:35:43 -0500

    finish Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+		for (int i=1; i <=10; i++){
+			System.out.println(i);
+		}
+
+		for (int x=10; x>=1; x--){
+			System.out.println(x);
+		}
+	}
+}
