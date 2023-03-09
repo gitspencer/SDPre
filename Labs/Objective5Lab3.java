668bdd64c12b0574afcbfc3989d6f4f7b4d6e198
Spencer Williams <srwilliams349@gmail.com>
2023-03-09 00:12:16 -0500

    finish Objective5Lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Please enter a number: ");
+		int userNum = scanner.nextInt();
+		
+		if(userNum > 0) {
+			System.out.println("The number is greater than 0");
+		if(userNum < 0)
+			System.out.println("The number is less than 0");	
+		if(userNum == 0)
+			System.out.println("The number is equal to 0");
+		}
+	}
+}
