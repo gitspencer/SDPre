d8d524018ebb5b80b83b41893ccecddb76d653cc
Spencer Williams <srwilliams349@gmail.com>
2023-03-08 20:39:48 -0500

    finish Objective3Lab3

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -1,6 +1,7 @@
 public class Objective3Lab3 {
 	public static void main(String[] args) {
 		int first=1+(int)(Math.random()*6), second=1+(int)(Math.random()*6), third=1+(int)(Math.random()*6);
-		System.out.println(first + second + third);
+		int sum = first + second + third;
+		System.out.println(first + " + " + second + " + " + third + " = " + sum);
 	}
 }
