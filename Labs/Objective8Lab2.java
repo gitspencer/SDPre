87ca9526ea18ef5e2624018c3ae66623cc69825a
Spencer Williams <srwilliams349@gmail.com>
2023-03-09 23:48:55 -0500

    finish Objective8Lab2

--- Labs/Objective8Lab2.java
+++ Labs/Objective8Lab2.java
@@ -1,9 +1,9 @@
 public class Objective8Lab2 {
 	public static void main(String[] args) {
 		int sum = 0;
-			for (int counter = 0; counter <= 20; counter ++){
-			sum = counter + sum;
-				System.out.println(sum);
+			for (int counter = 1; counter <= 20; counter ++){
+				sum += counter;
 			}
+		System.out.println(sum);
 	}
 }
