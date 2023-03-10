c08eb3727acfe001b429dd3af1f5a0b07ae31035
Spencer Williams <srwilliams349@gmail.com>
2023-03-09 23:46:07 -0500

    finish Objective7Lab4

--- Labs/Objective7Lab4.java
+++ Labs/Objective7Lab4.java
@@ -1,12 +1,12 @@
 public class Objective7Lab4 {
 	public static void main(String[] args) {
-		int counter = 0;
+		int counter = 1;
 		int sum = 0;
 
-			while(counter <= 19) {
-				counter = counter + 1;
-				sum = counter + sum;
-				System.out.println(sum);
+			while(counter <= 20) {
+				sum += counter;
+				counter += 1;
 			}
+		System.out.println(sum);
 	}
 }
